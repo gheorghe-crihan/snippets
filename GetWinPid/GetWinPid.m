aeae6c282469b2b9b3f47a4618b0a5c0cc843eb2
mac <mac@MacBook-Pro-mac-2.local>
2025-12-19 18:15:04 -0800

    - Pretty top-like look.

--- GetWinPid/GetWinPid.m
+++ GetWinPid/GetWinPid.m
@@ -8,20 +8,82 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreGraphics/CoreGraphics.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#define Log(fmt, ...) \
+fprintf(stderr, "%s\n", [[NSString stringWithFormat:(fmt), ##__VA_ARGS__] UTF8String])
+
+NSString *Right(NSString *s, NSInteger width, NSString *padSymbol)
+{
+    NSInteger padding = width - [s length];
+    if (padding < 0) padding = 0;
+
+    NSString *pad = [@"" stringByPaddingToLength:padding
+                                      withString:padSymbol
+                                 startingAtIndex:0];
+
+    return [pad stringByAppendingString:s];
+}
+
+NSString *Left(NSString *s, NSInteger width, NSString *padSymbol)
+{
+    return [s stringByPaddingToLength:width
+                           withString:padSymbol
+                      startingAtIndex:0];
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         CFArrayRef wlBefore = CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements, kCGNullWindowID);
-        NSLog(@"Move target window\n");
+        Log(@"Move target window\n");
         sleep(5);
         CFArrayRef wlAfter = CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements, kCGNullWindowID);
 
         NSMutableSet *result = [NSMutableSet setWithArray:(__bridge NSArray *)wlBefore];
         [result minusSet:[NSSet setWithArray:(__bridge NSArray *)wlAfter]];
-        NSLog(@"\nList of windows that moved:");
-        NSLog(@"%@", result);
-        NSLog(@"\n");
+        Log(@"\nList of windows that moved:");
+        Log(@"%@ %@  %@\t[Title] SubTitle\n",
+              Right(@"PID",7,@" "),
+              Right(@"WinID",5,@" "),
+              Left(@"x,y,w,h",21,@" ")
+              );
+        Log(@"%@ %@  %@ %@\n",
+              Right(@"-",7,@"-"),
+              Right(@"-",5,@"-"),
+              Left(@"-",21,@"-"),
+              @"\t-------------------------------------------"
+              );
+
+        for (NSObject* v in result) {
+            //NSLog(@"Class <%@>\n", [v class]);
+            NSNumber *OwnerPID = [v valueForKey:@"kCGWindowOwnerPID"] ?: @"?";
+            NSNumber *WindowNumber = [v valueForKey:@"kCGWindowNumber"] ?: @"?";
+            NSString *OwnerName = [v valueForKey:@"kCGWindowOwnerName"] ?: @"";
+            NSString *WindowName = [v valueForKey:@"kCGWindowName"] ?: @"";
+            NSObject *WindowBounds = [v valueForKey:@"kCGWindowBounds"];
+            NSString *Coords = @"";
+            if (WindowBounds!=nil) {
+                Coords = [NSString stringWithFormat:@"{%@,%@,%@,%@}",
+                          [WindowBounds valueForKey:@"X"],
+                          [WindowBounds valueForKey:@"Y"],
+                          [WindowBounds valueForKey:@"Width"],
+                          [WindowBounds valueForKey:@"Height"]
+                          ];
+            }
+
+            if ([WindowName length]!=0)
+                WindowName = [@" " stringByAppendingString:WindowName];
+            Log(@"%@ %@ %@\t[%@] %@\n",
+                  Right([OwnerPID stringValue],7,@" "),
+                  Right([WindowNumber stringValue],5,@" "),
+                  Left(Coords,21,@" "),
+                  OwnerName,
+                  WindowName
+                  );
+        }
+        //Log(@"%@", result);
+        Log(@"\n");
     }
     return 0;
 }
